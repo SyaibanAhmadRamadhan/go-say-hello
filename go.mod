37c239ab553a97b92ec27e16dcea3a38bdf9322f
iban.rama <syaiban95@gmail.com>
2023-01-17 16:24:23 +0700

    major changes say hello

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/SyaibanAhmadRamadhan/go-say-hello
+module github.com/SyaibanAhmadRamadhan/go-say-hello/v2
 
 go 1.19
--- go_say_hello.go
+++ go_say_hello.go
@@ -1,7 +1,5 @@
 package go_modules
 
-import "fmt"
-
-func SayHello(name string) {
-	fmt.Println("hello, nama saya", name)
+func SayHello(name string) string {
+	return "hello, nama saya " + name
 }
